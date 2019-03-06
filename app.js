57a729972dda3ed76da9f0c2bd09d418828b1068
Roshan Adhikari <nahsorad@gmail.com>
2019-03-06 19:40:48 +0545

    Use process.env for keys

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const schedule = require('node-schedule');
 const Twit = require('twit');
 
 let T = new Twit({
-    consumer_key:         '11qOqZhqLELjsw9kHD49wSB2h',
-    consumer_secret:      't4zDozD3qyduEZ6npJSa17lC6ggeZWSPw19srgRxw0QiZeTOzt',
-    access_token:         '1069965908230242306-BxoE4riF5Plv7gPDryIGAl64kGFH5T',
-    access_token_secret:  'VXLBjGxD1V0i7bHjNOXFbRNY2e0bbxA5SbvivSVOqI28u',
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
 });
