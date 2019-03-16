aca8dcaf558889290583ea884846da52a1b5cbf6
Roshan Adhikari <nahsorad@gmail.com>
2019-03-16 08:38:49 +0545

    Fix duplicate tweet

--- app.js
+++ app.js
@@ -17,17 +17,36 @@ let T = new Twit({
 
 function tweetIt(){
     let tweetMsg = '';
+    var lenTweetMsg;
     wotd.wordThink().then(data => {
         let word = data.word;
         let meaning = data.meaning;
         tweetMsg = `The word for today is '${word}', ${meaning}`;
+        lenTweetMsg = tweetMsg.length;
+        if(lenTweetMsg > 280)
+            tweetMsg = 'Have a good day everyone!';
         console.log(tweetMsg);
 
-        T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
-            if(err)
-                console.log(err);
+        // Search if the tweet has already been tweeted by the account
+        T.get('search/tweets', { q: '"' + tweetMsg +'" from:botterMan16', count: 100 }, function(err, data, response) {
+            if(data.statuses.length == 0){
+                // Tweet does not exist as of yet
+                console.log("Tweet IS NOT a duplicate, going to be tweeted!");
+                T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
+                    if(err) console.log(err, tweetMsg);
+                });
+            }
+            else if(data.statuses.length > 0 && (tweetMsg == 'Have a good day everyone!')){
+                console.log("Tweet IS a duplicate: " + tweetMsg + ", going to be tweeted!");
+                T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
+                    if(err) console.log(err, tweetMsg);
+                });
+            }
+            else{
+                // Tweet already exists
+                console.log("Tweet IS a duplicate, not going to be tweeted!");
+            }
         });
-
     });
 }
 
