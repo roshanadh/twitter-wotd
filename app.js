930f687e6adf99e21b5388a32c50c7bc374e951b
Roshan Adhikari <nahsorad@gmail.com>
2021-08-24 11:24:42 +0545

    Migrate to Wordnik API

--- app.js
+++ app.js
@@ -1,82 +1,144 @@
-const wotd = require('words-of-the-day');
-const Twit = require('twit');
-const express = require('express');
+const Twit = require("twit");
+const express = require("express");
+const https = require("https");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.WORDNIK_API_KEY;
+
+const url = `https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${API_KEY}`;
+
+const maxLength = 280;
 
 let T = new Twit({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+	consumer_key: process.env.BOT_CONSUMER_KEY,
+	consumer_secret: process.env.BOT_CONSUMER_SECRET,
+	access_token: process.env.BOT_ACCESS_TOKEN,
+	access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
+	timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+	strictSSL: true, // optional - requires SSL certificates to be valid.
 });
 
-function tweetIt(){
-    let preText = '';
-    let tweetMsg = '';
-    let hashTag = ' #WordOfTheDay';
-    let hashTags = ' #DailyWords #WordOfTheDay';
-    var lenPreText, lenTweetMsg, lenWordMeaning, lenHashTag, lenHashTags;
-    wotd.wordThink().then(data => {
-        let word = data.word;
-        let meaning = data.meaning;
-        
-        preText = "The word for today is '', ";
+const getWordOfTheDay = async () => {
+	https
+		.get(url, (res) => {
+			let data = "";
 
-        lenPreText = preText.length;
-        lenWordMeaning = word.length + meaning.length;
-        lenHashTag = hashTag.length;
-        lenHashTags = hashTags.length;
+			res.on("data", (chunk) => {
+				data += chunk;
+			});
 
-        if(lenPreText + lenWordMeaning + lenHashTags <= 280)
-            tweetMsg = `The word for today is '${word}', ${meaning}${hashTags}`;         
-        else if(lenPreText + lenWordMeaning + lenHashTags > 280 && lenPreText + lenWordMeaning + lenHashTag <= 280)
-            tweetMsg = `The word for today is '${word}', ${meaning}${hashTag}`;
-        else if(lenPreText + lenWordMeaning + lenHashTag > 280 && lenPreText + lenWordMeaning <= 280)
-            tweetMsg = `The word for today is '${word}', ${meaning}`;
-        else
-            tweetMsg = 'Have a good day everyone!';
+			res.on("end", () => {
+				data = JSON.parse(data);
+				return data;
+			});
+		})
+		.on("error", (error) => {
+			throw new Error(
+				"Error occurred while fetching word of the day:",
+				error
+			);
+		});
+};
 
-        lenTweetMsg = tweetMsg.length;
-        console.log(tweetMsg, lenTweetMsg);
-        
-        // Search if the tweet has already been tweeted by the account
-        T.get('search/tweets', { q: '"' + tweetMsg +'" from:daytheofword', count: 100 }, function(err, data, response) {
-            if(typeof data.statuses == 'undefined' || data.statuses.length == 0){
-                // Tweet does not exist as of yet
-                console.log("Tweet IS NOT a duplicate, going to be tweeted!");
-                T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
-                    if(err) console.log(err, tweetMsg);
-                });
-            }
-            else if(data.statuses.length > 0 && (tweetMsg == 'Have a good day everyone!')){
-                console.log("Tweet IS a duplicate: " + tweetMsg + ", going to be tweeted!");
-                T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
-                    if(err) console.log(err, tweetMsg);
-                });
-            }
-            else{
-                // Tweet already exists
-                console.log("Tweet IS a duplicate, not going to be tweeted!");
-            }
-        });
-    });
-}
+const getFitTweet = () => {
+	const response = await getWordOfTheDay();
+	const preText = `Word for today is '${word},'`;
+	const word = response.word;
+	const partOfSpeech = response.definitions[0].partOfSpeech;
+	const meaning = response.definitions[0].text;
+	const example = response.examples[0].text;
+	const hashTags = "#DailyWords #WordOfTheDay";
+	const hashTag = "#WordOfTheDay";
 
-app.all('/daytheofword', (req, res) => {
-    res.status(200).send("This is the response!");
-    tweetIt();
+	let tweet = "";
+	if (
+		`${preText} ${partOfSpeech}: ${meaning}. "${example}" ${hashTags}`
+			.length <= maxLength
+	) {
+		tweet = `${preText} ${partOfSpeech}: ${meaning}. "${example}" ${hashTags}`;
+	} else if (
+		`${preText} ${partOfSpeech}: ${meaning}. "${example}" ${hashTag}`
+			.length <= maxLength
+	) {
+		tweet = `${preText} ${partOfSpeech}: ${meaning}. "${example}" ${hashTag}`;
+	} else if (
+		`${preText} ${partOfSpeech}: ${meaning}. ${hashTags}`.length <=
+		maxLength
+	) {
+		tweet = `${preText} ${partOfSpeech}: ${meaning}. ${hashTags}`;
+	} else if (
+		`${preText} ${partOfSpeech}: ${meaning}. ${hashTag}`.length <= maxLength
+	) {
+		tweet = `${preText} ${partOfSpeech}: ${meaning}. ${hashTag}`;
+	} else if (
+		`${word}, ${partOfSpeech}: ${meaning}. "${example}" ${hashTags}`
+			.length <= maxLength
+	) {
+		tweet = `${word}, ${partOfSpeech}: ${meaning}. ${hashTags}`;
+	} else if (
+		`${word}, ${partOfSpeech}: ${meaning}. "${example}" ${hashTag}`
+			.length <= maxLength
+	) {
+		tweet = `${word}, ${partOfSpeech}: ${meaning}. "${example}" ${hashTag}`;
+	} else if (
+		`${word}, ${partOfSpeech}: ${meaning}. ${hashTag}`.length <= maxLength
+	) {
+		tweet = `${word}, ${partOfSpeech}: ${meaning}. ${hashTag}`;
+	} else {
+		tweet = "Have a great day folks!";
+	}
+	return tweet;
+};
+
+const doesTweetExist = (tweet) => {
+	// Search if the tweet has already been tweeted by the account
+	T.get(
+		"search/tweets",
+		{ q: '"' + tweet + '" from:daytheofword', count: 100 },
+		function (err, data, response) {
+			if (
+				typeof data.statuses == "undefined" ||
+				data.statuses.length == 0
+			) {
+				// Tweet does not exist as of yet
+				return false;
+			} else {
+				// Tweet already exists
+				return true;
+			}
+		}
+	);
+};
+
+const tweetIt = async () => {
+	const tweet = getFitTweet();
+
+	const hasBeenTweeted = doesTweetExist(tweet);
+
+	if (tweet !== "Have a great day folks!" && hasBeenTweeted) {
+		console.log("Tweet ALREADY EXISTS!");
+		tweet = "Have a great day folks!";
+		console.log(`Going to tweet: ${tweet}`);
+	} else {
+		console.log("Tweet is original!");
+		console.log(`Going to tweet: ${tweet}`);
+	}
+
+	T.post(
+		"statuses/update",
+		{ status: tweet },
+		function (err, data, response) {
+			if (err) console.log(err, tweet);
+		}
+	);
+};
+
+app.all("/daytheofword", (req, res) => {
+	res.status(200).send("This is the response!");
+	tweetIt();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is now listening to port ${PORT}`);
-})
-
-
-
-
-
-
+	console.log(`Server is now listening to port ${PORT}`);
+});
